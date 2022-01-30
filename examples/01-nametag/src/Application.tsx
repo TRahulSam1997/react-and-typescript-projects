f091e607f6073ed3aa3ec7f0e29b8a369b839012
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-01-31 10:50:32 +1100

    css styling types update

--- examples/01-nametag/src/Application.tsx
+++ examples/01-nametag/src/Application.tsx
@@ -1,22 +1,58 @@
-type NameTagProps = {
-  name: string;
-};
+// type NameTagProps = {
+//   name: string;
+// };
 
-const NameTag = (props: NameTagProps) => {
-  return (
-    <main>
-      <header>
-        <h1>Hello</h1>
-        <p>My Name Is</p>
-      </header>
-      <section className="display-name">
-        <p>{props.name}</p>
-      </section>
-      <footer />
-    </main>
-  );
-};
+// const NameTag = (props: NameTagProps) => {
+//   return (
+//     <main>
+//       <header>
+//         <h1>Hello</h1>
+//         <p>My Name Is</p>
+//       </header>
+//       <section className="display-name">
+//         <p>{props.name}</p>
+//       </section>
+//       <footer />
+//     </main>
+//   );
+// };
 
-const Application = () => <NameTag name="Rahul" />;
+// const Application = () => <NameTag name="Rahul" />;
 
-export default Application;
+// export default Application;
+
+/**
+ * Things you could try:
+ *
+ * JSX.Element;
+ * JSX.Element | JSX.Element[];
+ * React.ReactNode;
+ * React.ReactChildren;
+ * React.ReactChild[];
+ */
+
+ type BoxProps = { children: React.ReactNode, style?: React.CSSProperties };
+
+ const Box = ({ children, style = {} }: BoxProps) => {
+   return (
+     <section style={{ padding: "1em", border: "5px solid purple", ...style}}>
+       {children}
+     </section>
+   );
+ };
+
+ export default function Application() {
+   return (
+     <Box>
+       Just a string.
+       <p>Some HTML that is not nested.</p>
+       <Box style={{ borderColor: 'red' }}>
+         <h2>Another React component with one child.</h2>
+       </Box>
+       <Box>
+         <h2>A nested React component with two children.</h2>
+         <p>The second child.</p>
+       </Box>
+     </Box>
+   );
+ }
---
+++ examples/01-nametag/src/scratch.ts
@@ -0,0 +1,5 @@
+function add(a: number, ...otherProps:number[]){
+
+}
+
+export {}
