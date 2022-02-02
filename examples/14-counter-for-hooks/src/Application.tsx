47f4bd876c2577c6c3af0c54f366a19613d11f65
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-02-02 14:33:06 +1100

    react hook typs

--- examples/14-counter-for-hooks/src/Application.tsx
+++ examples/14-counter-for-hooks/src/Application.tsx
@@ -1,12 +1,31 @@
+import { ChangeEvent, useState } from 'react';
+
+type CounterProps = {
+  incident: string;
+}
+
+type CounterState = {
+  count: number;
+}
+
+const inc = (count: number) => ( count + 1 );
+const dec = (count: number) => ( count - 1 );
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+
+const changeCount = (event: ChangeEvent<HTMLInputElement>) => {
+  setCount( +event.target.value );
+}
+
   return (
     <main className="Counter">
       <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
+      <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={() => setCount(inc)}>Increment</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(dec)}>Decrement</button>
       </section>
       <section className="controls">
         <form onSubmit={() => {}}>
